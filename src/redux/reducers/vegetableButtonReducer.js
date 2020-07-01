497560f9845554239f2a72ab0f49c301bc7255fb
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-01 12:05:26 -0500

    Moved BUTTON IMAGE store to REDUX store

--- src/redux/reducers/vegetableButtonReducer.js
+++ src/redux/reducers/vegetableButtonReducer.js
@@ -1,7 +1,24 @@
-const vegetableButtonReducer = (state = [], action) => {
+import carrot from "../../images/carrot.png";
+import bellPepper from "../../images/bell-pepper.png";
+import lettuce from "../../images/lettuce.png";
+import beans from "../../images/beans.png";
+import peas from "../../images/peas.png";
+import corn from "../../images/corn.png";
+
+const vegetableButtonReducer = (
+  state = [
+    { img: carrot, id: 1, type: "Carrot" },
+    { img: bellPepper, id: 2, type: "Bell Pepper" },
+    { lettuce: lettuce, id: 3, type: "Lettuce" },
+    { beans: beans, id: 4, type: "Beans" },
+    { peas: peas, id: 5, type: "Peas" },
+    { corn: corn, id: 6, type: "Corn" },
+  ],
+  action
+) => {
   switch (action.type) {
-    case "SET_VEGETABLE_BUTTON_PICTURES":
-      return action.payload;
+    case "GET_VEGETABLE_BUTTON_PICTURES":
+      return state;
     case "UNSET_VEGGIES":
       return [];
     default:
