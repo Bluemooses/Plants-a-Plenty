58ab7d4b0dcce5433601765de0132909332b2e19
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-05 14:58:04 -0500

    Async set up

--- server/routes/garden.router.js
+++ server/routes/garden.router.js
@@ -12,17 +12,26 @@ router.get("/", (req, res) => {
 /**
  * POST route template
  */
-router.post("/create-garden", (req, res, next) => {
+router.post("/create-garden", async (req, res, next) => {
   //   console.log(req.body.user);
   console.log(req.body.payload);
+  const connection = await pool.connect();
   userID = req.body.payload;
+  try {
+    //   console.log(next);
+    const queryText = `INSERT INTO "GardenBed" ("name") VALUES ($1) RETURNING "id";`;
 
-  //   console.log(next);
-  const queryText = `INSERT INTO "GardenBed" ("name") VALUES ($1) RETURNING "id";`;
-  pool
-    .query(queryText, [userID])
-    .then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500));
+    pool.query(queryText, [userID]);
+    await connection.query("COMMIT");
+    console.log("HYPE");
+    res.sendStatus(201);
+  } catch (error) {
+    await connection.query("ROLLBACK");
+    console.log("ERROR IN SERVER GARDENBED POST", error);
+    res.sendStatus(500);
+  } finally {
+    connection.release();
+  }
 });
 
 module.exports = router;
